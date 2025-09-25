3705cc08d9ecc9cd8b718ba44f46606cf69cd01b
jumagulova <143736589+jumagulova@users.noreply.github.com>
2025-09-25 17:57:05 +0500

    Add Facebook Meta Pixel site-wide in layout head with noscript fallback

--- app/layout.tsx
+++ app/layout.tsx
@@ -93,8 +93,25 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationStructuredData) }}
         />
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '777216705021542');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body className={`${inter.className} bg-gray-50`}>
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }} src="https://www.facebook.com/tr?id=777216705021542&ev=PageView&noscript=1" alt="" />
+        </noscript>
         <Header />
         <main>{children}</main>
         <Footer />
